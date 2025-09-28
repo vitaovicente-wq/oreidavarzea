d4318e95ff4f94db6b34a9bf2d1784ba4bd46df7
vitaovicente-wq <vitao.vicente@gmail.com>
2025-09-28 10:40:14 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,4 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('seasonData')) {
         btnContinueGame.disabled = false;
     }
+    document.getElementById('btnNewGame').addEventListener('click', () => {
+        // Limpa o jogo antigo ANTES de ir para a tela de escolha
+        localStorage.clear(); 
+    });
 });
