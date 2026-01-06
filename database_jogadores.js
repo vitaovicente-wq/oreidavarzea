a20659036580963d4936c7c507f01b2e3807880b
vitaovicente-wq <vitao.vicente@gmail.com>
2026-01-06 09:49:04 -0300

    Create database_jogadores.js

---
+++ database_jogadores.js
@@ -0,0 +1,32 @@
+const PlayersDB = {
+    "Flamengo": [
+        { nome: "Rossi", pos: "GOL", forca: 83, idade: 30, carac: "Pegador de Pênaltis" }, // Especialista
+        { nome: "Léo Ortiz", pos: "ZAG", forca: 85, idade: 30, carac: "Construtor" }, // Sai jogando bem
+        { nome: "Léo Pereira", pos: "ZAG", forca: 82, idade: 29, carac: "Xerife" },
+        { nome: "Ayrton Lucas", pos: "ZAG", forca: 81, idade: 28, carac: "Apoio" }, // Lateral ofensivo
+        { nome: "De la Cruz", pos: "MEI", forca: 88, idade: 28, carac: "Maestro" },
+        { nome: "Pulgar", pos: "MEI", forca: 82, idade: 31, carac: "Cão de Guarda" },
+        { nome: "Pedro", pos: "ATA", forca: 90, idade: 28, carac: "Artilheiro" },
+        { nome: "Bruno Henrique", pos: "ATA", forca: 80, idade: 35, carac: "Veloz" }
+    ],
+    "Palmeiras": [
+        { nome: "Weverton", pos: "GOL", forca: 84, idade: 37, carac: "Paredão" },
+        { nome: "Gómez", pos: "ZAG", forca: 85, idade: 32, carac: "Líder" }, // Capitão
+        { nome: "Piquerez", pos: "ZAG", forca: 83, idade: 27, carac: "Construtor" },
+        { nome: "Mayke", pos: "ZAG", forca: 79, idade: 32, carac: "Defensivo" },
+        { nome: "Estêvão", pos: "ATA", forca: 88, idade: 18, carac: "Veloz" },
+        { nome: "Flaco López", pos: "ATA", forca: 82, idade: 25, carac: "Pivô" }
+    ],
+    "Man City": [
+        { nome: "Ederson", pos: "GOL", forca: 89, idade: 31, carac: "Joga com os Pés" }, // O melhor nisso
+        { nome: "Rodri", pos: "MEI", forca: 93, idade: 29, carac: "Maestro" },
+        { nome: "Haaland", pos: "ATA", forca: 94, idade: 25, carac: "Artilheiro" }
+    ],
+    "Corinthians": [
+        { nome: "Hugo Souza", pos: "GOL", forca: 80, idade: 26, carac: "Pegador de Pênaltis" },
+        { nome: "Fagner", pos: "ZAG", forca: 76, idade: 36, carac: "Apoio" },
+        { nome: "André Ramalho", pos: "ZAG", forca: 81, idade: 33, carac: "Xerife" },
+        { nome: "Memphis Depay", pos: "ATA", forca: 87, idade: 31, carac: "Pivô" }
+    ]
+    // ... adicione as características aos outros times seguindo essa lógica
+};
